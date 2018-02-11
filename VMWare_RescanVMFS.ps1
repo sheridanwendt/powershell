5036252437e38243547f62180187645b0392f337
Sheridan Wendt <sheridan.wendt@gmail.com>
2018-02-10 19:57:53 -0700

    VMWare: Rescan for VMFS volumes
    This script will instruct each host in the cluster to rescan it's existing Host Bus Adapters (HBAs) for new VMFS volumes

---
+++ VMWare_RescanVMFS.ps1
@@ -0,0 +1,22 @@
+﻿# ********************************************************************************
+#
+# Script Name: VMWare_RescanVMFS.ps1
+# Version: 1.0
+# Author: Sheridan Wendt
+# Date: 11/21/2017
+# Applies to: ESXi Hosts
+#
+# Description: This script will instruct each host in the cluster to rescan it's  
+# Host Bus Adapters (HBAs) for new VMFS volumes on LUNs
+#
+# ********************************************************************************
+
+# Set variables
+$Cluster = "ClusterName"
+$vCenter = "vcenter.domain.local"
+
+# Connect to vCenter server
+Connect-VIServer $vCenter
+
+# Check the members of the cluster. For each member, rescan VMFS volumes
+Get-Cluster -Name $Cluster | Get-VMHost | Get-VMHostStorage -RescanVmfs
