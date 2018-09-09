5499e06cfb840dfc159458eaec1ee477b4e6a581
Sheridan Wendt <sheridan.wendt@gmail.com>
2018-09-09 14:18:11 -0600

    Update Azure-NewVM.ps1

--- Azure-NewVM.ps1
+++ Azure-NewVM.ps1
@@ -1,4 +1,4 @@
-﻿# ********************************************************************************
+# ********************************************************************************
 #
 # Script Name: Azure-NewVM.ps1
 # Version: 1.0
@@ -17,8 +17,8 @@ $ResourceGroupName = "RG2"
 $Location = "SouthCentralUS"
 
 ## Compute
-$VMName = "DC01"
-$ComputerName = "DC01"
+$VMName = "APP1"
+$ComputerName = "APP1"
 $VMSize = "Standard_B1ms"
 $OSDiskName = "$ComputerName" + "_OSDisk"
 
@@ -28,7 +28,7 @@ $Random = -join ((65..90) + (97..122) | Get-Random -Count 4 | % {[char]$_})
 $StorageAccountName = "Storage" + $VMName + "$Random"
 $StorageAccountName = $StorageAccountName.ToLower()
 $StorageType = "Standard_GRS"
-###type options currently include Premium_LRS, Standard_GRS, Standard_LRS, Standard_RAGRS, Standard_ZRS
+### type options currently include Premium_LRS, Standard_GRS, Standard_LRS, Standard_RAGRS, Standard_ZRS
 $StorageAccount = New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageAccountName -Type $StorageType -Location $Location
 
 ## Network
@@ -37,24 +37,19 @@ $Subnet1Name = "Subnet1"
 $VNetName = "VNet" + $VMName
 $VNetAddressPrefix = "10.1.0.0/16"
 $VNetSubnetAddressPrefix = "10.1.1.0/24"
-
-# Actions
-## Create Public New IP
+### Create Public New IP
 $pIP = New-AzureRmPublicIpAddress -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Dynamic
-
-## Create a Subnet Configuration
+### Create a Subnet Configuration
 $SubnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name $Subnet1Name -AddressPrefix $VNetSubnetAddressPrefix
-
-## Create a Virtual Network
+### Create a Virtual Network
 $VNet = New-AzureRmVirtualNetwork -Name $VNetName -ResourceGroupName $ResourceGroupName -Location $Location -AddressPrefix $VNetAddressPrefix -Subnet $SubnetConfig
-
-## Create a NIC
+### Create a NIC
 $Interface = New-AzureRmNetworkInterface -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $VNet.Subnets[0].Id -PublicIpAddressId $pIP.Id
 
 ## Create OS credentials
 $Credential = Get-Credential
 
-## Create Virtual Machine
+## Add Variables to Virtual Machine Configuration
 $VirtualMachine = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
 $VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
 $VirtualMachine = Set-AzureRmVMSourceImage -VM $VirtualMachine -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2016-Datacenter -Version "latest"
@@ -62,4 +57,5 @@ $VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $Interfa
 $OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + "vhds/" + $OSDiskName + ".vhd"
 $VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -CreateOption FromImage
 
+## Create the VM
 New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine
